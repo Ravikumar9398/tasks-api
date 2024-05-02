fd498b83af9d715c4789455174d4412619fe942b
Ravikumar9398 <ravikumar.yesupogu9@gmail.com>
2024-05-02 07:06:59 +0530

    api 2 commit

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,98 +4,114 @@ const Tasks = require("../model/tasks");
 
 // Create a new Task
 router.post("/add", async (req, res) => {
-  const { title, description, status, employee_id } = req.body;
+  try {
+    const { title, description, status, employee_id } = req.body;
 
-  // checking register user
-  const isEmployee = await User.findOne({ employee_id });
-  if (!isEmployee) {
-    return res.send({
-      status: 400,
-      message: "Employee not found",
+    // checking register user
+    const isEmployee = await User.findOne({ employee_id });
+    if (!isEmployee) {
+      return res.send({
+        status: 400,
+        message: "Employee not found",
+      });
+    }
+
+    // checking Accessibility
+    const isAdmin =
+      isEmployee.employment_type === "Admin" ||
+      isEmployee.employment_type === "Manager";
+    if (!isAdmin) {
+      return res.send({
+        status: 400,
+        message: "You are not authorized to add task",
+      });
+    }
+
+    // checking the task already exists
+    const isTaskExists = await Tasks.findOne({ title });
+    if (isTaskExists) {
+      return res.send({
+        status: 400,
+        message: "Task Already Exists",
+      });
+    }
+
+    const task = new Tasks({
+      title,
+      description,
+      status,
     });
-  }
+    await task.save();
 
-  // checking Accessibility
-  const isAdmin =
-    isEmployee.employment_type === "Admin" ||
-    isEmployee.employment_type === "Manager";
-  if (!isAdmin) {
-    return res.send({
-      status: 400,
-      message: "You are not authorized to add task",
+    res.send({
+      status: 200,
+      message: "Task added successfully",
     });
+  } catch (error) {
+    res.json({ error: error.message });
   }
-
-  // checking the task already exists
-  const isTaskExists = await Tasks.findOne({ title });
-  if (isTaskExists) {
-    return res.send({
-      status: 400,
-      message: "Task Already Exists",
-    });
-  }
-
-  const task = new Tasks({
-    title,
-    description,
-    status,
-  });
-  await task.save();
-
-  res.send({
-    status: 200,
-    message: "Task added successfully",
-  });
 });
 
 // Get all Tasks
 router.get("/", async (req, res) => {
-  const tasks = await Tasks.find();
-  res.send({
-    status: 200,
-    data: tasks,
-  });
+  try {
+    const tasks = await Tasks.find();
+    res.send({
+      status: 200,
+      data: tasks,
+    });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 });
 
 // Get Task by id
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const task = await Tasks.findById(id);
-  if (!task) {
-    return res.send({
-      status: 400,
-      message: "Task not found",
+  try {
+    const { id } = req.params;
+    const task = await Tasks.findById(id);
+    if (!task) {
+      return res.send({
+        status: 400,
+        message: "Task not found",
+      });
+    }
+    res.send({
+      status: 200,
+      data: task,
     });
+  } catch (err) {
+    res.json({ error: err.message });
   }
-  res.send({
-    status: 200,
-    data: task,
-  });
 });
 
 // Update Task by id
 
 router.put("/update/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, description, status } = req.body;
-  const task = await Tasks.findByIdAndUpdate(id, {
-    title,
-    description,
-    status,
-  });
-  if (!task) {
-    return res.send({
-      status: 400,
-      message: "Task not found",
+  try {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+    const task = await Tasks.findByIdAndUpdate(id, {
+      title,
+      description,
+      status,
     });
-  }
+    if (!task) {
+      return res.send({
+        status: 400,
+        message: "Task not found",
+      });
+    }
 
-  await task.save();
-  res.send({
-    status: 200,
-    message: "Task updated successfully",
-  });
+    await task.save();
+    res.send({
+      status: 200,
+      message: "Task updated successfully",
+    });
+  } catch (error) {
+    res.json({ error: error });
+  }
 });
 
 // Delete Task by id
